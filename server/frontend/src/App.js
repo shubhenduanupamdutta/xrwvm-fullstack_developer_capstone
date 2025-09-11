a0e7311c9dd5fc91695b8e8e91ef11f680f79493
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2025-09-11 18:28:31 +0530

    Search Car frontend and django link

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Dealer from "./components/Dealers/Dealer";
 import Dealers from "./components/Dealers/Dealers";
 import PostReview from "./components/Dealers/PostReview";
+import SearchCars from "./components/Dealers/SearchCars";
 import LoginPanel from "./components/Login/Login";
 import RegisterPanel from "./components/Register/Register";
 
@@ -13,6 +14,7 @@ function App() {
 			<Route path="/dealers" element={<Dealers />} />
 			<Route path="/dealer/:id" element={<Dealer />} />
 			<Route path="/postreview/:id" element={<PostReview />} />
+			<Route path="/searchcars/:id" element={<SearchCars />} />
 		</Routes>
 	);
 }
